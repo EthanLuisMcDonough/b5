50a2d5882deee12f09787c3b89097a3bfac4b30e
Ethan L. McDonough <elmcdonough@comcast.net>
2022-07-12 22:55:09 -0500

    Add static files

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 use actix_web::{get, web, App, HttpServer, Responder};
 
+use actix_files::Files;
+
 #[get("/")]
 async fn home() -> impl Responder {
     "IT REALLY IS"
 }
 
-#[actix_web::main] // or #[tokio::main]
+#[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(home))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(home)
+            .service(Files::new("/static", "./static"))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
